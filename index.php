504d6ea9e111d0cc03e6fdb8e93fa6a1b41880e8
Rômulo cesar <rcsolarsilva@hotmail.com>
2020-02-21 13:53:24 -0300

    Criado o forgot

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ use \Banco\Model\User;
 
 $app = new Slim();
 
-$app->config('debug', true);
+$app->config("debug", true);
 
 $app->get('/', function() {
 
@@ -21,7 +21,7 @@ $app->get('/', function() {
 
 });
 
-$app->get('/admin', function() {
+$app->get("/admin", function() {
 
 	User::verifyLogin();
 
@@ -31,7 +31,7 @@ $app->get('/admin', function() {
 
 });
 
-$app->get('/admin/login', function() {
+$app->get("/admin/login", function() {
 
 	$page = new PageAdmin([
 
@@ -44,7 +44,7 @@ $app->get('/admin/login', function() {
 
 });
 
-$app->post('/admin/login', function() {
+$app->post("/admin/login", function() {
 
 	User::login($_POST["login"], $_POST["password"]);
 
@@ -53,13 +53,147 @@ $app->post('/admin/login', function() {
 
 });
 
-$app->get('/admin/logout', function() {
+$app->get("/admin/logout", function() {
 
 	User::logout();
 
 	header("Location: /admin/login");
 
 	exit;
+
+});
+
+$app->get("/admin/users", function() {
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+
+		"users"=>$users
+
+	));
+
+});
+
+$app->get("/admin/users/create", function() {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get("/admin/users/:iduser", function($iduser) {
+
+	User::verifyLogin();
+ 
+    $user = new User();
+ 
+    $user->get((int)$iduser);
+ 
+    $page = new PageAdmin();
+ 
+    $page ->setTpl("users-update", array(
+        "user"=>$user->getValues()
+    ));
+
+});
+
+$app->post("/admin/users/create", function(){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+
+ 		"cost"=>12
+
+ 	]);	
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get("/admin/forgot", function() {
+
+	$page = new PageAdmin([
+
+		"header"=>false,
+		"footer"=>false
+
+	]);
+
+	$page->setTpl("forgot");
+
+});
+
+$app->post("/admin/forgot", function() {
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+
+});
+
+$app->get("/admin/forgot/sent", function() {
+
+	$page = new PageAdmin([
+
+		"header"=>false,
+		"footer"=>false
+
+	]);	
+
+	$page->setTpl("forgot-sent");
 
 });
 
--- vendor/banco/php-classes/src/Model/User.php
+++ vendor/banco/php-classes/src/Model/User.php
@@ -4,10 +4,21 @@
 
 	use \Banco\DB\Sql;
 	use \Banco\Model;
+	use \Banco\Mailer;
 
 	class User extends Model{
 
 		const SESSION = "User";
+
+		const SECRET = "BancoPhp7_Secret";
+
+		const SECRET_IV = "BancoPhp7_Secret";
+
+		const ERROR = "UserError";
+
+		const ERROR_REGISTER = "UserErrorRegister";
+
+		const SUCCESS = "UserSucesss";
 
 		public static function login($login, $password)
 		{
@@ -71,6 +82,150 @@
 		{
 
 			$_SESSION[User::SESSION] = NULL;
+
+		}
+
+		public static function listAll()
+		{
+
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+		}
+
+		public function save()
+		{
+
+			$sql = new Sql();
+			
+			$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+
+				":desperson"=>$this->getdesperson(),
+				":deslogin"=>$this->getdeslogin(),
+				":despassword"=>$this->getdespassword(),
+				":desemail"=>$this->getdesemail(),
+				":nrphone"=>$this->getnrphone(),
+				":inadmin"=>$this->getinadmin()
+
+			));
+
+			$this->setData($results[0]);
+
+		}
+
+		public function get($iduser)
+		{
+		 
+			$sql = new Sql();
+			 
+			$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
+
+			 		":iduser"=>$iduser
+					 
+					 ));
+			 
+			$data = $results[0];
+			 
+			$this->setData($data);
+		 
+		}
+
+		public function update()
+		{
+
+						$sql = new Sql();
+			
+			$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+
+				":iduser"=>$this->getiduser(),
+				":desperson"=>$this->getdesperson(),
+				":deslogin"=>$this->getdeslogin(),
+				":despassword"=>$this->getdespassword(),
+				":desemail"=>$this->getdesemail(),
+				":nrphone"=>$this->getnrphone(),
+				":inadmin"=>$this->getinadmin()
+
+			));
+
+			$this->setData($results[0]);
+
+		}
+
+		public function delete()
+		{
+
+			$sql = new Sql();
+
+			$sql->query("CALL sp_users_delete(:iduser)", array(
+
+				":iduser"=>$this->getiduser()
+
+			));
+
+		}
+
+		public static function getForgot($email, $inadmin = true)
+		{
+
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email;", array(
+				"email"=>$email
+			));
+
+			if (count($results) === 0)
+			{
+				throw new \Exception("Não foi possível recuperar a senha.");
+				
+			} else {
+
+				$data = $results[0];
+
+				$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+
+					":iduser"=>$data["iduser"],
+					":desip"=>$_SERVER["REMOTE_ADDR"]
+
+				));
+
+				if (count($results2) === 0)
+				{
+				
+					throw new \Exception("Não foi possível recuperar a senha.");					
+					
+				} else {
+
+					$dataRecovery = $results2[0];
+
+					$code = openssl_encrypt($dataRecovery['idrecovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+					$code = base64_encode($code);
+
+					if ($inadmin === true) {
+
+						$link = "http://www.ecommerce.com.br/admin/forgot/reset?code=$code";
+
+					} else {
+
+						$link = "http://www.ecommerce.com.br/forgot/reset?code=$code";
+						
+					}					
+
+					$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir senha da aula do PHP 7", "forgot", array(
+
+						"name"=>$data["desperson"],
+						"link"=>$link
+
+					));
+
+					$mailer->send();
+
+					return $link;
+
+				}
+
+			}
 
 		}
 
