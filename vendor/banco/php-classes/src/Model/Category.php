bfa6dc93818e38b66940f8b8629ce8099ee90ae0
Rômulo cesar <rcsolarsilva@hotmail.com>
2020-03-04 10:45:08 -0300

    Site - Categorias

--- vendor/banco/php-classes/src/Model/Category.php
+++ vendor/banco/php-classes/src/Model/Category.php
@@ -31,6 +31,8 @@
 
 			$this->setData($results[0]);
 
+			Category::updateFile();
+
 		}
 
 		public function get($idcategory)
@@ -58,6 +60,25 @@
 				':idcategory'=>$this->getidcategory()
 
 			]);
+
+			Category::updateFile();
+
+		}
+
+		public static function updateFile()
+		{
+
+			$categories = Category::listall();
+
+			$html = [];
+
+			foreach ($categories as $row) {
+				
+				array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+
+			}
+
+			file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."categories-menu.html", implode('', $html));
 
 		}
 
