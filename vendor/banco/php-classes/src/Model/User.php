a328d40e4a6dbc661d0d41a68211b64fc468b029
Rômulo cesar <rcsolarsilva@hotmail.com>
2020-03-19 10:35:49 -0300

    Minha Conta

--- vendor/banco/php-classes/src/Model/User.php
+++ vendor/banco/php-classes/src/Model/User.php
@@ -370,6 +370,31 @@
 
 		}
 
+		public static function setSuccess($msg)
+	{
+
+		$_SESSION[User::SUCCESS] = $msg;
+
+	}
+
+	public static function getSuccess()
+	{
+
+		$msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+
+		User::clearSuccess();
+
+		return $msg;
+
+	}
+
+	public static function clearSuccess()
+	{
+
+		$_SESSION[User::SUCCESS] = NULL;
+
+	}
+
 		public static function getPasswordHash($password)
 			{
 
