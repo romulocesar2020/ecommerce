85b0a0992e5d855a0345f668b750998e46322579
Rômulo cesar <rcsolarsilva@hotmail.com>
2020-05-11 16:06:54 -0300

    Finalizar Pedido - Boleto

--- vendor/banco/php-classes/src/Model/Order.php
+++ vendor/banco/php-classes/src/Model/Order.php
@@ -17,15 +17,18 @@
 			$sql = new Sql();
 
 			$results = $sql->select("CALL sp_orders_save(:idorder, :idcart, :iduser, :idstatus, :idaddress, :vltotal)", [
+
 				':idorder'=>$this->getidorder(),
 				':idcart'=>$this->getidcart(),
 				':iduser'=>$this->getiduser(),
 				':idstatus'=>$this->getidstatus(),
 				':idaddress'=>$this->getidaddress(),
 				':vltotal'=>$this->getvltotal()
+
 			]);
 
 			if (count($results) > 0) {
+				
 				$this->setData($results[0]);
 			}
 
